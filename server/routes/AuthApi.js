92224ae5405a1cd985ef2b21fe7e9855fb353e88
Anurag Jain <jainanurag941@gmail.com>
2022-11-30 11:39:27 +0530

    Login User Part 1

--- server/routes/AuthApi.js
+++ server/routes/AuthApi.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import User from "../models/User.js";
 const router = Router();
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 router.post("/register", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -25,6 +26,30 @@ router.post("/register", async (req, res) => {
 
   await user.save();
   res.status(201).json({ message: "User is successfully created" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(406).json({ message: "Invalid Credentials." });
+    return;
+  }
+
+  const matched = await bcrypt.compare(password, user.password);
+  if (!matched) {
+    res.status(406).json({ message: "Invalid Credentials." });
+    return;
+  }
+
+  const payload = {
+    username: email,
+    _id: user._id,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET);
+  res.json({ message: "Successfully Logged In", token });
 });
 
 export default router;
