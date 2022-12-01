7ea43d3198f20dadb111ff374bcc44a7d40e0f60
Anurag Jain <jainanurag941@gmail.com>
2022-12-01 22:53:05 +0530

    React Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import AppBar from "./components/AppBar";
 import { Outlet } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getUser } from "./store/auth.js";
+import { useEffect } from "react";
 
 function App() {
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  console.log(auth);
+
   return (
     <>
       <AppBar />
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,15 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import router from "./routes.js";
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
